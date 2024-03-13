eda04249462e0425e7cbf5c2e67aeccbcb38d6a3
Parker-Parker <pagrant@wpi.edu>
2024-03-12 23:48:17 -0400

    reeeeeeeeEEEEEEEEEEEEEEEe

--- AAAAAAHHHHHH/compile_script.sh
+++
@@ -1,2 +0,0 @@
-# zip -r ../BUILD\?/game.zip ../SRC\?\?/*
-zip -r ../BUILD/game.zip ../SRC/*
---
+++ compile_script.sh
@@ -0,0 +1,4 @@
+# zip -r ../BUILD\?/game.zip ../SRC\?\?/*
+# zip -r ./BUILD/game.zip ../SRC/
+# zip -r ../BUILD\?/game.zip ../SRC\?\?/*
+zip -r test.zip ./SRC   
