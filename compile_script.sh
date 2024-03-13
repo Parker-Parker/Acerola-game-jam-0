521424a669bc3dd3761471e526aeaf66a6d23d42
Parker-Parker <pagrant@wpi.edu>
2024-03-13 00:04:53 -0400

    it works

--- compile_script.sh
+++ compile_script.sh
@@ -1,4 +1,10 @@
 # zip -r ../BUILD\?/game.zip ../SRC\?\?/*
 # zip -r ./BUILD/game.zip ../SRC/
 # zip -r ../BUILD\?/game.zip ../SRC\?\?/*
-zip -r test.zip ./SRC   
+# zip -r ./BUILD/game.love ./SRC/*   
+# zip -r ./BUILD/game.zip ./SRC/*
+  
+cd ./SRC; zip -r ../BUILD/game.love ./* ; cd .. 
+cd ./SRC; zip -r ../BUILD/game.zip ./*  ; cd ..
+
+
