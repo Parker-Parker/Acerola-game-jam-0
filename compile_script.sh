5bda3eef52282b47c9cf91c7275f83d35ab8cf99
Parker-Parker <pagrant@wpi.edu>
2024-03-13 00:34:45 -0400

    build works

--- compile_script.sh
+++ compile_script.sh
@@ -5,6 +5,9 @@
 # zip -r ./BUILD/game.zip ./SRC/*
   
 cd ./SRC; zip -r ../BUILD/game.love ./* ; cd .. 
-cd ./SRC; zip -r ../BUILD/game.zip ./*  ; cd ..
-
+# cd ./SRC; zip -r ../BUILD/game.zip ./*  ; cd ..
+echo "cleaning win"; cd ./BUILD/Win;ls; rm ./*; rm -r ./*; cd ../..
+echo "Extracting win";unzip -j ./BUILD/love-11.5-win64.zip  -d ./BUILD/Win
+echo "Compiling win";cat ./BUILD/Win/love.exe ./BUILD/game.love > ./BUILD/Win/SuperGame.exe
+chmod +x ./BUILD/Win/SuperGame.exe
 
