1e8875fa050a91dde43afb1d4aafdefb392e7fe0
Parker-Parker <pagrant@wpi.edu>
2024-03-13 20:20:39 -0400

    capitalized

--- SRC/main.lua
+++ SRC/main.lua
@@ -4,36 +4,37 @@ require("character")
 
 -- Character.
 
-frames = 0
-framerate = 0
-framerate_lasttime = 0
+Frames = 0
+Framerate = 0
+Framerate_lasttime = 0
+
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
-    love.graphics.print(looptime, 400, 400)
-    love.graphics.print("Framerate: " .. framerate, 650, 20)
+    love.graphics.print(Looptime, 400, 400)
+    love.graphics.print("Framerate: " .. Framerate, 650, 20)
 
     Character:draw()
-    frames = frames + 1
+    Frames = Frames + 1
 end
 
-looptime = 0
+Looptime = 0
 
-function love.update(dt) 
-    looptime = looptime + dt
+function love.update(dt)
+    Looptime = Looptime + dt
     local mousex, mousey = love.mouse.getPosition()
     -- Character:moveAbs(mousex, mousey)
     Character:moveAbsIfMin(mousex, mousey, 10)
 
 
-    
-    if looptime - framerate_lasttime > 1 then
-        framerate = frames/(looptime-framerate_lasttime)
-        framerate_lasttime = looptime
-        frames = 0
+
+    if Looptime - Framerate_lasttime > 1 then
+        Framerate = Frames/(Looptime-Framerate_lasttime)
+        Framerate_lasttime = Looptime
+        Frames = 0
     end
-    if looptime > 10000 then
-        looptime = 0
-        frames = 0
-        framerate_lasttime = 0
+    if Looptime > 10000 then
+        Looptime = 0
+        Frames = 0
+        Framerate_lasttime = 0
     end
 end
