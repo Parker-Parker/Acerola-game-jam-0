32a0eee47c8c1f7806a8799712c0181c206e648b
Parker-Parker <pagrant@wpi.edu>
2024-03-13 21:35:33 -0400

    move controller

--- SRC/character.lua
+++ SRC/character.lua
@@ -20,6 +20,19 @@ Character.x = 0
 Character.y = 0
 Character.theta = 0
 
+Character.accelEnum = {IDLE = 0, BOOST = 1}
+Character.steerEnum = {IDLE = 0, RIGHT = 1, LEFT = 2}
+Character.minSpeed = 10
+Character.maxSpeed = 800
+Character.accelRate = 300
+Character.decelRate = 80
+Character.brakeRate = 30
+Character.turnRate = 2
+
+
+Character.speed = Character.minSpeed
+
+
 function Character:moveAbs(newX, newY)
     self.theta = math.atan2(newY-self.y,newX-self.x)
     self.x = newX
@@ -93,6 +106,7 @@ function Character:tf()
 end
 
 
+
 function Character:draw()
     love.graphics.setColor(1,1,1)
     -- love.graphics.setColor(255,255,255)
@@ -100,4 +114,40 @@ function Character:draw()
     self:tf()
     love.graphics.polygon("fill", self.vertTableTransformed)
     -- self.x
+end
+
+function Character:collisionManager(x0,y0,x1,y1)
+    --store travel and collisions
+
+end
+
+function Character:moveRel(dt, accel, steer) 
+    local cX = math.cos(Character.theta)
+    local cY = math.sin(Character.theta)
+    local newX = Character.x  + dt*Character.speed*cX -- + accel_factor  -- TODO: fix dt
+    local newY = Character.y  + dt*Character.speed*cY -- + accel_factor  -- TODO: fix dt  
+
+    if accel == Character.accelEnum.BOOST then
+        Character.speed = Character.speed +Character.accelRate*dt -- TODO: fix dt
+        if Character.speed > Character.maxSpeed then
+            Character.speed = Character.maxSpeed
+        end
+    else
+        Character.speed = Character.speed -Character.decelRate*dt -- TODO: fix dt
+        if Character.speed < Character.minSpeed then
+            Character.speed = Character.minSpeed
+        end
+    end
+
+    if steer == Character.steerEnum.LEFT then
+        Character.theta = Character.theta - Character.turnRate*dt -- TODO: fix dt
+    elseif steer == Character.steerEnum.RIGHT then
+        Character.theta = Character.theta + Character.turnRate*dt -- TODO: fix dt
+    end
+
+    Character:collisionManager(Character.x,Character.y,
+                                newX,       newY)
+    Character.x = newX
+    Character.y = newY
+
 end
--- SRC/main.lua
+++ SRC/main.lua
@@ -8,10 +8,14 @@ Frames = 0
 Framerate = 0
 Framerate_lasttime = 0
 
+accelMode = 0
+steerMode = 0
+
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
     love.graphics.print(Looptime, 400, 400)
     love.graphics.print("Framerate: " .. Framerate, 650, 20)
+    love.graphics.print("Steer:" .. steerMode.." Boost:"..accelMode, 650, 50)
 
     Character:draw()
     Frames = Frames + 1
@@ -19,12 +23,25 @@ end
 
 Looptime = 0
 
+Character:moveAbs(400,400)
+
 function love.update(dt)
     Looptime = Looptime + dt
-    local mousex, mousey = love.mouse.getPosition()
-    -- Character:moveAbs(mousex, mousey)
-    Character:moveAbsIfMin(mousex, mousey, 10)
+    -- local mousex, mousey = love.mouse.getPosition()
+    -- -- Character:moveAbs(mousex, mousey)
+    -- Character:moveAbsIfMin(mousex, mousey, 10)
 
+    accelMode = (love.keyboard.isDown("w") or love.keyboard.isDown("up") ) and
+                            Character.accelEnum.BOOST or Character.accelEnum.IDLE
+
+    local right = love.keyboard.isDown("d") or love.keyboard.isDown("right")
+    local left = love.keyboard.isDown("a") or love.keyboard.isDown("left")
+
+    steerMode = (left == right)  and Character.steerEnum.IDLE or 
+                            left and Character.steerEnum.LEFT or Character.steerEnum.RIGHT
+
+
+    Character:moveRel(dt,accelMode,steerMode)
 
 
     if Looptime - Framerate_lasttime > 1 then
